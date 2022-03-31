9d3a667fb23ef281ebb5a7a2715bccd033a432c2
Renan Girotto <46816071+renangirotto@users.noreply.github.com>
2022-03-30 21:37:07 -0300

    feat: add assistant and first animation
    # Assistant
    
    Add assistant component, this component will handle the circular frame and the assistant avatar base styles
    
    With that I add the first animation, a "default" or "live" state, that will handle simple head movements, shadow relatives and blink actions
    
    Maybe this first animation evolve, but for this first stage it is fine
    
    # Global CSS
    
    Add some CSS global variables and resets. I will use :root because I think it will be easier to control them with JS if I need

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-
+import Assistant from "@/components/Assistant/Assistant.vue"
 </script>
 
 <template>
-  
+  <div class="center">
+    <Assistant />
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+.center {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
+  button {
+    margin-top: 50px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "@/assets/css/reset.css";
+import "@/assets/css/variables.css";
+
 createApp(App).mount("#app");
